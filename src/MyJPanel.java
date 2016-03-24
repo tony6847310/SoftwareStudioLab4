d5e5e02dc4f34e91cd5a12ae1ae416ac7161e882
perezsam <escalantesammy@gmail.com>
2016-03-24 22:19:29 +0800

    Update MyJPanel.java
    sam MyJPanel

--- src/MyJPanel.java
+++ src/MyJPanel.java
@@ -4,30 +4,49 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 
 import javax.swing.*;
+import javax.swing.GroupLayout.Alignment;
 
 
 public class MyJPanel extends JPanel implements MouseMotionListener{
 	
-	private Square square = new Square(100);
-	private int centerX, centerY;
+	private Square square = new Square(120);
+	private JPanel squarePanel1=new JPanel();
 	
 	public MyJPanel(){
 		addMouseMotionListener(this);
+		
+		squarePanel1.setLocation(0, 0);
+		squarePanel1.setBounds(0, 0, square.getShapeWidth(), square.getShapeHeight());
+
+		GroupLayout squarePanel1Layout = new GroupLayout(squarePanel1);
+        squarePanel1.setLayout(squarePanel1Layout);
+        squarePanel1Layout.setHorizontalGroup(squarePanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGap(0, square.getShapeWidth(), Short.MAX_VALUE)
+        );
+        squarePanel1Layout.setVerticalGroup(squarePanel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGap(0, square.getShapeHeight(), Short.MAX_VALUE)
+        );
+		
+        this.add(squarePanel1);
+		
+		
 	}
+	
+	
 	protected void paintComponent(Graphics g){
 		super.paintComponent(g);
-		// TODO Draw square and fill it with random color decided by method getRandomColor()
-		// You should use fillRect()
-		g.setColor(this.square.getRandomColor());
-		g.fillRect(this.square.getCenterX(centerX), this.square.getCenterY(centerY), this.square.getShapeWidth(), this.square.getShapeHeight());
+		
+		squarePanel1.setBackground(square.getRandomColor());
+		
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		// TODO Get mouse dragged position and change suqare's position
-		centerX = e.getX();
-		centerY = e.getY();
-		repaint();
+		squarePanel1.setLocation(e.getX()-square.getShapeWidth()/2, e.getY()-square.getShapeWidth()/2);
+		//centerX = e.getX();
+		//centerY = e.getY();
+		squarePanel1.repaint();
 	}
 
 	@Override
